8ecd8e78ba1cdfd309889864bc85b819487f1d9c
trevordilley <trevor@twentyideas.com>
2020-01-19 23:01:24 -0800

    Now the Gui doesn't cause massive numbers of rerenders of the game

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,16 +1,73 @@
-import React, {useLayoutEffect} from "react"
+import React, {Component} from "react"
 import * as Phaser from "phaser"
+import * as lodash from 'lodash'
+const hostElementId = "game"
+interface GameProps {
+ config: Phaser.Types.Core.GameConfig
+}
 
-export const Game: React.FC<{ config: Phaser.Types.Core.GameConfig }> =
-    (props) => {
-        const {config, children} = props
+export const Game: React.FC<GameProps> = (props) => {
 
-        const hostElementId = 'game'
-        // requires useLayoutEffect because this is where we attach the game
-        // canvas to the DOM
-        useLayoutEffect(() => {
-            new Phaser.Game({...config, parent: hostElementId})
-        })
+    const {config, children} = props
+    return (   <>
+        <GameGui config={config}>
+            {children}
+        </GameGui>
+        <GameContainer config={config}/>
+    </>)
+}
+
+const GameGui: React.FC<GameProps> = (props) => {
+
+    const {config, children} = props
+
+    return (
+        <div
+            id={`${hostElementId}-gui-container`}
+            style={{
+                // Set origin (0,0) to match origin of
+                // the game canvas
+                position: "absolute",
+
+                // So the gui respects the positioning
+                // of the canvas
+                width: config.width,
+                height: config.height,
+
+                // Allow click through this container
+                // div
+                pointerEvents: "none"
+            }}
+        >
+            <div
+                id={`${hostElementId}-gui`}
+                style={{
+                    // The actual GUI, don't allow click through
+                    // (so Buttons and stuff actually capture the clicks)
+                    pointerEvents: "auto",
+
+                    border: "1px solid"
+                }}>
+                {children}
+            </div>
+        </div>
+        )
+}
+
+export class GameContainer extends Component<GameProps> {
+
+    // TODO: This should probably update if we change the
+    // any part of the actual config.
+    shouldComponentUpdate(nextProps:GameProps , nextState:{}) {
+        return !lodash.isEqual(this.props, nextProps)
+    }
+
+    componentDidMount() {
+        const {config} = this.props
+        new Phaser.Game({...config, parent: hostElementId})
+    }
+
+    render() {
 
         // The games (the `children` prop) GUI will render
         // as the first child and the game Canvas will be
@@ -22,34 +79,10 @@ export const Game: React.FC<{ config: Phaser.Types.Core.GameConfig }> =
         // top-left, with x increasing going right and
         // y increasing going down.
         return (
-            <div id={hostElementId}>
-                <div
-                    id={`${hostElementId}-gui-container`}
-                    style={{
-                        // Set origin (0,0) to match origin of
-                        // the game canvas
-                        position: "absolute",
+            <div id={hostElementId} >
+            </div>
+        )
 
-                        // So the gui respects the positioning
-                        // of the canvas
-                        width: config.width,
-                        height: config.height,
-
-                        // Allow click through this container
-                        // div
-                        pointerEvents: 'none'
-                    }}
-                >
-                    <div
-                        id={`${hostElementId}-gui`}
-                        style={{
-                        // The actual GUI, don't allow click through
-                        // (so Buttons and stuff actually capture the clicks)
-                        pointerEvents: 'auto'
-                    }}>
-                        {children}
-
-                    </div>
-                </div>
-            </div>)
     }
+}
+
